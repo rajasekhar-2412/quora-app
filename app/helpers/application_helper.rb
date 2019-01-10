e9b8c2883d38dddce62dbd87eff007e736d86657
Rajasekhar B <raja@progton.com>
2019-01-10 16:59:25 +0530

    changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def formatted_date date
     date.strftime("%b %d, %Y %l:%M%P")
   end
+
+  def author user
+  	'Answered by' + user&.first_name.to_s + user&.last_name.to_s
+  end
 end
